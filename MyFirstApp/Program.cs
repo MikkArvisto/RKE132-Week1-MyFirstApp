7a5501a2bbb8a149356db338ef03eca88a67b9a0
Mikk Arvisto <marvisto@tktk.ee>
2022-09-07 15:37:18 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// This is a comment to my code
+
+//rakendus küsib nime
+//rakendus tervitab nimepidi
+
+Console.WriteLine("Enter your name:");
+
+//string - sõne
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", " + userName + "!");
+
+//string interpolation
+
+Console.WriteLine($"Hello, {userName}!");
